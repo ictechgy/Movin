d562016740429cceecc9fd5ebba5debb40d29f9c
ictechgy <ictechgy@gmail.com>
2022-02-21 01:11:37 +0900

    🎨 [Style]: 코드의 일관성을 위해 enum 위치 변경

--- Movin/Model/RequestDTO/MovieDetailRequestDTO.swift
+++ Movin/Model/RequestDTO/MovieDetailRequestDTO.swift
@@ -8,14 +8,14 @@
 struct MovieDetailRequestDTO: Encodable {
     let language: String?
     let desiredAdditionalResponse: [String]?
-}
-
-extension MovieDetailRequestDTO {
+    
     enum CodingKeys: String, CodingKey {
         case language
         case desiredAdditionalResponse = "append_to_response"
     }
-    
+}
+
+extension MovieDetailRequestDTO {
     func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encode(language, forKey: .language)
