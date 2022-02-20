0faa99eef91624e4da0c32c33cbc1576ded817f4
ictechgy <ictechgy@gmail.com>
2022-02-21 00:47:12 +0900

    ✨ [Feat]: 상세 화면에 대한 도메인 타입 구현

---
+++ Movin/Domain/ResponseEntity/MovieDetail.swift
@@ -0,0 +1,37 @@
+//
+//  MovieDetail.swift
+//  Movin
+//
+//  Created by JINHONG AN on 2022/02/21.
+//
+
+struct MovieDetail {
+    let isAdultMovie: Bool?
+    let backdropPath: String?
+    let genreCategories: [Genre]?
+    let id: Int?
+    let originalTitle: String?
+    let overview: String?
+    let posterPath: String?
+    let productionCompanies: [Company]?
+    let releaseDate: String?
+    let runningTime: Int?
+    let status: String?
+    let tagline: String?
+    let title: String?
+    let numberOfUsersEvaluated: Int?
+    let averageUserScore: Double?
+    let participants: ParticipantsDTO?
+}
+
+struct Genre {
+    let id: Int?
+    let name: String?
+}
+
+struct Company {
+    let name: String?
+    let id: Int?
+    let logoPath: String?
+    let originCountry: String?
+}
