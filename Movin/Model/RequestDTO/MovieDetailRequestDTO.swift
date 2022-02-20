98ace7f9c9c3998d28a4d9a0d44598d559e2c52c
ictechgy <ictechgy@gmail.com>
2022-02-20 18:22:07 +0900

    ✨ [Feat]: 영화 상세 정보 요청을 위한 타입 정의

---
+++ Movin/Model/RequestDTO/MovieDetailRequestDTO.swift
@@ -0,0 +1,25 @@
+//
+//  MovieDetailRequestDTO.swift
+//  Movin
+//
+//  Created by JINHONG AN on 2022/02/17.
+//
+
+struct MovieDetailRequestDTO: Encodable {
+    let language: String?
+    let desiredAdditionalResponse: [String]?
+}
+
+extension MovieDetailRequestDTO {
+    enum CodingKeys: String, CodingKey {
+        case language
+        case desiredAdditionalResponse = "append_to_response"
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(language, forKey: .language)
+        try container.encode(desiredAdditionalResponse?.joined(separator: ","),
+                             forKey: .desiredAdditionalResponse)
+    }
+}
